b17bd19ca0e5ef0184dcd3b48a91912ce832c1cc
MelKori <mieldo@gmail.com>
2022-07-12 16:21:03 +0300

    fix upsert; make register-clause! reusable

--- src/honeysql_h2/core.clj
+++ src/honeysql_h2/core.clj
@@ -5,45 +5,49 @@
   (:import (java.sql Date Time Timestamp)))
 
 ;; honeysql extensions
+(defn h2ext []
+  (do
+    (sql/register-clause! :records
+                          (fn [_ [pk xs]]
+                            (let [cols-1 (keys (first xs))
+                                  cols-n (into #{} (mapcat keys) xs)
+                                  cols (if (= (set cols-1) cols-n) cols-1 cols-n)
+                                  [sqls params]
+                                  (reduce (fn [[sql params] [sqls' params']]
+                                            [(conj sql (str "(" (s/join ", " sqls') ")"))
+                                             (if params' (into params params') params')])
+                                          [[] []]
+                                          (map (fn [m]
+                                                 (sql/format-expr-list
+                                                   (map #(get m %) cols)))
+                                               xs))]
+                              (into [(str "("
+                                          (s/join ", "
+                                                  (map #(sql/format-entity % {:drop-ns true}) cols))
+                                          ") "
+                                          (when pk (str (sql/sql-kw :key) "("
+                                                        (if (sequential? pk)
+                                                          (s/join "," (map #(sql/format-entity % {:drop-ns true}) pk))
+                                                          (sql/format-entity pk {:drop-ns true}))
+                                                        ") "))
+                                          (sql/sql-kw :values)
+                                          " "
+                                          (s/join ", " sqls))]
+                                    params)))
+                          nil)
 
-(sql/register-clause! :records
-                      (fn [_ [pk xs]]
-                        (let [cols-1 (keys (first xs))
-                              cols-n (into #{} (mapcat keys) xs)
-                              cols (if (= (set cols-1) cols-n) cols-1 cols-n)
-                              [sqls params]
-                              (reduce (fn [[sql params] [sqls' params']]
-                                        [(conj sql (str "(" (s/join ", " sqls') ")"))
-                                         (if params' (into params params') params')])
-                                      [[] []]
-                                      (map (fn [m]
-                                             (sql/format-expr-list
-                                               (map #(get m %) cols)))
-                                           xs))]
-                          (into [(str "("
-                                      (s/join ", "
-                                              (map #(sql/format-entity % {:drop-ns true}) cols))
-                                      ") "
-                                      (when pk (str (sql/sql-kw :key) "("
-                                                    (if (sequential? pk)
-                                                      (s/join "," (map #(sql/format-entity % {:drop-ns true}) pk))
-                                                      (sql/format-entity pk {:drop-ns true}))
-                                                    ") "))
-                                      (sql/sql-kw :values)
-                                      " "
-                                      (s/join ", " sqls))]
-                                params)))
-                      nil)
+    (sql/register-clause! :merge-into
+                          (fn [clause x]
+                            (let [[sql & params]
+                                  (if (ident? x)
+                                    (sql/format-expr x)
+                                    (sql/format-dsl x))]
+                              (into [(str (sql/sql-kw clause) " " sql)] params)))
+                          :records)                         ; <!> MERGE INTO ... ... KEY(_pk) VALUES ...
+    ;(println ">> H2 clauses registered")
+    true))
 
-(sql/register-clause! :merge-into
-                      (fn [clause x]
-                        (let [[sql & params]
-                              (if (ident? x)
-                                (sql/format-expr x)
-                                (sql/format-dsl x))]
-                          (into [(str (sql/sql-kw clause) " " sql)] params)))
-                      :records)                             ; <!> MERGE INTO ... ... KEY(_pk) VALUES ...
-
+(defonce ^:static ^:private h2-inited (h2ext))
 ;; Protocol extensions
 
 (extend-protocol IResultSetReadColumn
@@ -65,6 +69,6 @@
 
 (defn upsert [data table & [pk]]
   (-> {:merge-into table
-       :vals       [pk data]}
+       :records    [pk data]}
       sql/format))
 

307e365228e1fb696a8144808eb018f723540d93
MelKori <mieldo@gmail.com>
2022-08-01 01:24:23 +0300

    update project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject updcon/honeysql-h2 "0.0.2"
+(defproject ai.z7/honeysql-h2 "0.0.3"
   :description "The H2 dialect extension for HoneySQL library"
   :url "https://github.com/updcon/honeysql-h2"
   :license {:name "Built In Project License"

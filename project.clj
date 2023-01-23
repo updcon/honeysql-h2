dd101ef24e0e4db3afe43d1706a3ab974112d3da
MelKori <mieldo@gmail.com>
2023-01-23 16:59:19 +0200

    upgrade honeysql builtin library

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/updcon/honeysql-h2"
   :license {:name "Built In Project License"
             :url  "https://github.com/updcon/honeysql-h2/LICENSE"}
-  :dependencies [[com.github.seancorfield/honeysql "2.4.947"]
+  :dependencies [[com.github.seancorfield/honeysql "2.4.969"]
                  [com.h2database/h2 "2.1.214"]
                  [org.clojure/java.jdbc "0.7.12"]])

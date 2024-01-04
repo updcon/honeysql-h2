f09c77903f466c60df8dd2079efcde4c45657277
MelKori <mieldo@gmail.com>
2024-01-04 17:17:49 +0200

    update deps

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/updcon/honeysql-h2"
   :license {:name "Built In Project License"
             :url  "https://github.com/updcon/honeysql-h2/LICENSE"}
-  :dependencies [[com.github.seancorfield/honeysql "2.4.969"]
-                 [com.h2database/h2 "2.1.214"]
+  :dependencies [[com.github.seancorfield/honeysql "2.5.1103"]
+                 [com.h2database/h2 "2.2.224"]
                  [org.clojure/java.jdbc "0.7.12"]])
